51c5d586a90aabe2e4d22a05795cd27d61331c59
浜田　俊祐 <hamashun0820@icloud.com>
2024-09-21 20:01:06 +0900

    add: iconを追加

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ const manifest = defineManifest({
   version: "1.0.2",
   description:
     'YouTubeで音楽再生時に自動で再生速度を1倍に戻してくれるChrome拡張機能です。音楽の判定は概要欄の文字で判定しているため、言語が"日本語"、"English (US)"、"中文 (简体)"のいずれかに設定されていない場合は正常に動作しない可能性があります。',
+  icons: {
+    16: "icons/icon16.png",
+    48: "icons/icon48.png",
+    128: "icons/icon128.png",
+  },
   content_scripts: [
     {
       matches: ["https://www.youtube.com/*"],
