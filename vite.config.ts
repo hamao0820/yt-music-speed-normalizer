0658cb0482f34dac424c799ddeef944b81b90866
浜田　俊祐 <hamashun0820@icloud.com>
2024-09-21 20:04:53 +0900

    update: manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const manifest = defineManifest({
   name: "yt-music-speed-normalizer",
   version: "1.0.2",
   description:
-    'YouTubeで音楽再生時に自動で再生速度を1倍に戻してくれるChrome拡張機能です。音楽の判定は概要欄の文字で判定しているため、言語が"日本語"、"English (US)"、"中文 (简体)"のいずれかに設定されていない場合は正常に動作しない可能性があります。',
+    'YouTubeで音楽再生時に自動で再生速度を1倍に戻してくれるChrome拡張機能です。言語が"日本語"、"English (US)"、"中文 (简体)"のいずれかに設定されていない場合は正常に動作しない可能性があります。',
   icons: {
     16: "icons/icon16.png",
     48: "icons/icon48.png",
