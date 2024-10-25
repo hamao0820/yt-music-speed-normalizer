dadaffd9f73574d06028817fd32defd299a859a5
浜田　俊祐 <hamashun0820@icloud.com>
2024-10-25 09:35:33 +0900

    update manifest.json

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     manifest_version: 3,
     name: "yt-music-speed-normalizer",
-    version: "1.0.2",
+    version: "1.0.3",
     description:
       'YouTubeで音楽再生時に自動で再生速度を1倍に戻してくれるChrome拡張機能です。言語が"日本語"、"English (US)"、"中文 (简体)"のいずれかに設定されていない場合は正常に動作しない可能性があります。',
     icons: {
